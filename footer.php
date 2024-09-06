cc2032f45d2f538d2f90edb9606ebcb1dad3d6c5
Pinku rathour <pinkurathour2016@gmail.com>
2024-09-06 16:43:45 +0530

    Code push

---
+++ footer.php
@@ -0,0 +1,99 @@
+<footer>
+    <div class="container-fluid py-4">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-lg-6">
+                <div class="footer_links">
+                    <div class="footer_logo d-flex justify-content-center align-items-center py-5">
+
+                        <div class="" style="width: 6%;">
+                            <a href="index.html">
+                            <svg id="1501254508" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 357.44 357.44" class="svg u_1501254508" data-icon-custom="true"
+                                data-icon-name="HOUSE-ICON.svg">
+                                <defs>
+                                    <style>
+                                        .cls-1-1505595176 {
+                                            fill: #89af67;
+                                        }
+
+                                        .cls-2-1505595176 {
+                                            fill: none;
+                                            stroke: #fff;
+                                            stroke-miterlimit: 10;
+                                            stroke-width: 6.27px;
+                                        }
+
+                                        .cls-3-1505595176 {
+                                            fill: #fff;
+                                        }
+                                    </style>
+                                </defs>
+                                <circle class="cls-1-1505595176" cx="178.72" cy="178.72" r="178.72"></circle>
+                                <path class="cls-2-1505595176" d="M215.45,209.57" transform="translate(-36.6 -39.24)">
+                                </path>
+                                <path class="cls-3-1505595176"
+                                    d="M138.18,239.56v80.75h27V240A192.49,192.49,0,0,0,138.18,239.56Z"
+                                    transform="translate(-36.6 -39.24)"></path>
+                                <path class="cls-3-1505595176"
+                                    d="M214.87,92.48,138.18,200v27a195,195,0,0,1,31.14.83c27.55,2.52,89.08,16.59,96.06,92.29l.12.18h27V200ZM265.5,261.35c-28.08-43.23-79.05-51.21-100.36-52.57v-.12L215,138.73l50.5,70Z"
+                                    transform="translate(-36.6 -39.24)"></path>
+                            </svg>
+                            </a>
+                        </div>
+                        <div class="footer_content">
+
+                        </div>
+                    </div>
+                    <div class="footer_content d-flex justify-content-center align-items-center">
+                        <h5 class="text-light text-center w-5 lh-md">2303 N. RANDOLPH AVE. PORTLAND, OR 97227</h5>
+                    </div>
+                    <div class="text-center">
+                        <h5 class="py-4" style="color: #89af67;">
+                            (503) 493-7344</h5>
+                    </div>
+                    <div class="text-center d-flex justify-content-center align-items-center gap-3">
+                        <a href=""><span class="iconify text-decoration-none text-light fs-4"
+                                data-icon="mdi:instagram"></span></a>
+                        <a href=""> <span class="iconify text-decoration-none text-light fs-4"
+                                data-icon="ant-design:pinterest-filled"></span></a>
+                        <a href=""><span class="iconify text-decoration-none text-light fs-4"
+                                data-icon="ri:facebook-fill"></span></a>
+                        <a href=""><span class="iconify text-decoration-none text-light fs-4"
+                                data-icon="pajamas:twitter"></span></a>
+
+                    </div>
+                    <div class="text-center py-4">
+                        <ul
+                            class="list-unstyled d-flex flex-wrap justify-content-center align-items-center gap-xl-3 gap-3">
+                            <li><a href="#!" class="text-decoration-none fs-6 fw-light" style="color: #89af67;">JOIN
+                                    OUR
+                                    TEAM</a>
+                            </li>
+                            <li><a href="#!" class="text-decoration-none fs-6 fw-light" style="color: #89af67;">
+                                    CONTACT</a>
+                            </li>
+                            <li><a href="#!" class="text-decoration-none fs-6 fw-light" style="color: #89af67;">
+                                    BLOG </a>
+                            </li>
+                            <li><a href="#!" class="text-decoration-none fs-6 fw-light" style="color: #89af67;">
+                                    ICONIC
+                                    MAGAZINE
+                                </a></li>
+                            <li><a href="#!" class="text-decoration-none fs-6 fw-light" style="color: #89af67;">JOIN
+                                    OUR
+                                    ENEWS</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/iconify/2.0.0/iconify.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="/path/to/parallax.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ACRIFORM | Portland</title>
+    <link rel="shortcut icon" href="asset/images/fav.ico" type="image/x-icon">
+    <link rel="stylesheet" href="asset/css/bootstrap.css">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <link rel="stylesheet" href="asset/css/responsive.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg shadow-lg py-2 fixed-top bg-white">
+            <div class="container-fluid mx-lg-5 mx-1">
+                <a class="navbar-brand" href="index.html"><img src="asset/images/logo.webp" style="width: 200px;"></a>
+                <button class="navbar-toggler border-1 rounded-1 shadow-none" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="iconify fs-1 border-0" data-icon="heroicons:bars-3-bottom-right"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav d-flex gap-lg-5 gap-2 ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.html">About</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="service.html">Service</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="blog.html">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Contact Us</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    </header>
